8038202de3eb690fe97e05ebd391e808275feef8
alex <a99som@yandex.ru>
2021-04-07 03:53:01 +0300

    Алгоритмы общения, генерации UID работают

--- Client/main.cpp
+++ Client/main.cpp
@@ -14,31 +14,28 @@ int main() {
     std::string auth[2];
     auth[0] = "login";
     auth[1] = "password";
+    bool authentication[2] = {false};
     char buffer[128] = {'\0'};
 
     std::cout << "Sending login/password\n";
     zmq_send(socket, auth[0].c_str(), auth[0].length(), 0);
+    memset(buffer, '\0', 128);
     zmq_recv(socket, buffer, 128, 0);
-    std::string message;
-    memcpy(buffer, message.c_str(), strlen(buffer));
-    if(message == "login ok"){
-        zmq_send(socket, auth[1].c_str(), auth[0].length(), 0);
+    if((std::string)buffer == "login ok"){
+        authentication[0] = true;
+        zmq_send(socket, auth[1].c_str(), auth[1].length(), 0);
     }
+    memset(buffer, '\0', 128);
+    zmq_recv(socket, buffer, 128, 0);
 
-    /*for (int i = 0; i < 2; i++) {
-        zmq_send(socket, auth[i].c_str(), 128, 0);
-        zmq_recv(socket, buffer, 128, 0);
-        memcpy(buffer, result[i], 128);
-        for(int i = 0; i < 128; i++){
-            buffer[i] = '\0';
-        }
+    if((std::string)buffer == "password ok"){
+        authentication[0] = true;
+        zmq_send(socket, "Authentication done", 19, 0);
     }
-    if(((std::string)result[0] == "login done") && ((std::string)result[0] == "password done")){
-        zmq_send(socket, "Continue", 128, 0);
-    }
+    memset(buffer, '\0', 128);
     zmq_recv(socket, buffer, 128, 0);
     std::cout << std::string(buffer) << std::endl;
-*/
+
     zmq_close(socket);
     zmq_ctx_destroy(context);
 }
--- Proxy/main.cpp
+++ Proxy/main.cpp
@@ -13,25 +13,34 @@ int main() {
     void *context = zmq_ctx_new();
     void *socket = zmq_socket(context, ZMQ_REP);
     int rc = zmq_bind(socket, "tcp://*:4040");
+    std::string msg;
     assert(rc == 0);
     char buffer[128] = {'\0'};
     zmq_recv(socket, buffer, 128, 0);
-    std::string message;
-    memcpy(buffer, message.c_str(), strlen(buffer));
-    //Очистили буфер
-    memset(buffer, '\0', 128);
-    if(message == "login"){
-        zmq_send(socket, "login ok", 8, 0);
+    if((std::string)buffer == "login"){
+
+        Proxy px;
+        msg = std::to_string(px.genUserID());
+        msg += std::string(buffer);
+
+                zmq_send(socket, "login ok", 8, 0);
     }else{
         zmq_send(socket, "Authorisation denied", 20, 0);
     }
+    memset(buffer, '\0', 128);
     zmq_recv(socket, buffer, 128, 0);
-    message.clear();
-    memcpy(buffer, message.c_str(), strlen(buffer));
-    if(message == "password"){
+    if((std::string)buffer == "password"){
         zmq_send(socket, "password ok", 11, 0);
     }else{
         zmq_send(socket, "Authorisation denied", 20, 0);
     }
+    memset(buffer, '\0', 128);
+    zmq_recv(socket, buffer, 128, 0);
+    if((std::string)buffer == "Authentication done"){
+        std::cout << "Authentication done\n";
+    }
+    zmq_send(socket, msg.c_str(), msg.length(), 0);
+
+
 
 }
